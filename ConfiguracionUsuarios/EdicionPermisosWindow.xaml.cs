35c6ade75ac25ee5d1d601b3f5535cc184235dfa
José Rnmkr <milveintiuno@outlook.com>
2019-03-07 22:51:33 -0300

    zuuummm

--- ConfiguracionUsuarios/EdicionPermisosWindow.xaml.cs
+++ ConfiguracionUsuarios/EdicionPermisosWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using ConfiguracionUsuarios.DataAccessLayer;
 
 namespace ConfiguracionUsuarios
@@ -12,7 +13,7 @@ namespace ConfiguracionUsuarios
     {
         DBContext context = new DBContext();
         private int _idUsuario;
-
+        List<PermisoView> lvsource;
         public EdicionPermisosWindow(int idUsuario)
         {
             InitializeComponent();
@@ -25,21 +26,87 @@ namespace ConfiguracionUsuarios
             if (cbAplicaciones.SelectedValue != null)
             {
                 var _permisos = context.PermisoView.Where(w => w.FK_IDUsuario == _idUsuario).Select(s => s);
-                lvPermisos.ItemsSource = _permisos.Where(w => w.NombreAplicacion == cbAplicaciones.SelectedValue.ToString()).ToList();
-
+                lvsource = _permisos.Where(w => w.NombreAplicacion == cbAplicaciones.SelectedValue.ToString()).ToList();
+                lvPermisos.ItemsSource = lvsource;
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
-            //context.SaveChangesAsync();
+            //context.SaveChanges();
             MessageBox.Show("Todo piola!");
+            Close();
         }
 
-        private void ChkSelectAll_Click(object sender, RoutedEventArgs e)
+        //private void ChkSelectAll_Click(object sender, RoutedEventArgs e)
+        //{
+        //    CheckBox cbox = (CheckBox)sender;
+        //    List<PermisoView> lpv = new List<PermisoView>();
+
+        //    if (cbox.IsChecked == false)
+        //    {
+        //        foreach (var item in lvsource)
+        //        {
+        //             item.EstadoPermiso = false;
+        //            lpv.Add(item);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        foreach (var item in lvsource)
+        //        {
+        //            item.EstadoPermiso = true;
+        //        }
+        //        lvsource = lpv;
+        //        lvPermisos.ItemsSource = null;
+        //        lvPermisos.ItemsSource = lvsource;
+        //    }
+
+        //}
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DBContext context = new DBContext();
+            var listaPermisos = context.Permiso.Select(s => s);
+            List<PermisoUsuario> listaPermisosUsuario = context.PermisoUsuario.Where(w => w.FK_IDUsuario == _idUsuario).Select(s => s).ToList();
+            List<PermisoUsuario> listaNuevaPermisosUsuario = new List<PermisoUsuario>();
+            foreach (var item in listaPermisos)
+            {
+                if (listaPermisosUsuario.Where(w => w.FK_IDPermiso == item.IDPermiso).Any())
+                {
+
+                }
+                else
+                {
+                    listaNuevaPermisosUsuario.Add(new PermisoUsuario { FK_IDUsuario = _idUsuario, FK_IDPermiso = item.IDPermiso, EstadoPermiso = false });
+                }
+                
+            }
+            context.PermisoUsuario.AddRange(listaNuevaPermisosUsuario);
+            context.SaveChanges();
+            MessageBox.Show("Todo piola!");
+            lvPermisos.ItemsSource = null;
+            var _permisos = context.PermisoView.Where(w => w.FK_IDUsuario == _idUsuario).Select(s => s);
+            lvPermisos.ItemsSource = _permisos.Where(w => w.NombreAplicacion == cbAplicaciones.SelectedValue.ToString()).ToList();
+        }
+
+        private void chkSelectAll_Checked(object sender, RoutedEventArgs e)
         {
 
+            foreach (PermisoView item in lvPermisos.ItemsSource)
+            {
+                item.EstadoPermiso = true;
+            }
+            
+        }
+
+        private void chkSelectAll_Unchecked(object sender, RoutedEventArgs e)
+        {
+            foreach (PermisoView item in lvPermisos.ItemsSource)
+            {
+                item.EstadoPermiso = false;
+            }
         }
     }
 }
